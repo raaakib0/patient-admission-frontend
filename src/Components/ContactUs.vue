fe26045064ca081271e5cf46eb6b83d399ba039b
raaakib0 <raaakib0@gmail.com>
2025-10-31 20:46:10 +0600

    create contact us page

---
+++ src/Components/ContactUs.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="">
+<h1>Contact Us</h1>
+<p>please sent an emil to abc@gmail.comn</p>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
+import router from '../router'
 
 const app = createApp(App)
 app.use(createPinia())
