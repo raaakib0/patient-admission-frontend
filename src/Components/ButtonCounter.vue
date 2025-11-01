81bfb8ff45d4b08de717c87ee58693e754e81740
raaakib0 <raaakib0@gmail.com>
2025-11-01 12:15:36 +0600

    button count page added

---
+++ src/Components/ButtonCounter.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="count++" >Clicked {{ count }} times</button>
+</template>
+<script>
+export default{
+    setup(){
+        const count = ref(0);
+        return {count};
+    }
+};
+</script>
