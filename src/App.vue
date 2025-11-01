030a6c5c68eb6d41c3f9c2fed71d36d9adf93d53
raaakib0 <raaakib0@gmail.com>
2025-11-01 20:17:21 +0600

    Contact Cart

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
-
 <template>
   <!-- <contact-us></contact-us> -->
   <!-- <button-counter></button-counter> -->
-   <ContactUs
-   name="mr.x"
-   phone="01911111111"
-   :ownername="contactsOwner"
-   ></ContactUs>
+   Contact Owner Name : <input v-model="ownername"/>
+  <div class="row">
+    <div class="col-12" v-for="contact in contacts" :key="contact.name">
+      <ContactUs
+        :name="contact.name"
+        :phone="contact.phone"
+        :ownername="contact.ownername"
+        :email="contact.email"
+        :isFavorite="contact.isFavorite"
+      ></ContactUs>
+    </div>
+  </div>
+
   <ButtonCounter></ButtonCounter>
-  
+
   <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind + Vue 🎉</h1>
 
   <h1>You did it!</h1>
-  
 </template>
 
 <script setup>
-import ContactUs from './Components/ContactUs.vue';
-import ButtonCounter from './Components/ButtonCounter.vue';
-const contactsOwner = "abcd"
+import ContactUs from "./Components/ContactUs.vue";
+import ButtonCounter from "./Components/ButtonCounter.vue";
+import { reactive, ref } from "vue";
+const ownername = ref("abcd");
+const contacts = reactive([
+  {
+    name: "rahim",
+    phone: 1911111111,
+    ownername: ownername.value,
+    isFavorite: false,
+  },
+  {
+    name: "karim",
+    phone: 1911111111,
+    ownername: ownername.value,
+    isFavorite: false,
+  },
+  {
+    name: "jabaar",
+    phone: 1911111111,
+    ownername: ownername.value,
+    isFavorite: false,
+  },
+]);
 </script>
 
 <style scoped></style>
--- src/Components/ContactUs.vue
+++ src/Components/ContactUs.vue
@@ -1,15 +1,27 @@
 <template>
-    <div class="bg-info">
-<h1>Contact Us</h1>
-<p>Name: {{ name }}</p>
-<p>Email: {{ email }}</p>
-<p>Phone: {{ phone }}</p>
-<p class="float-end small " v-if="ownername!=''">this info for {{ ownername }}</p>
+  <div class="bg-info p-4 m-2 rounded">
+    <div class="row">
+      <div class="col-12">
+        <h1>Contact Us</h1>
+        <p>Name: {{ name }}</p>
+        <p>Email: {{ email }}</p>
+        <p>Phone: {{ phone }}</p>
+        <p class="float-end small" v-if="ownername != ''">
+          this info for {{ ownername }}
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import {ref, defineProps} from "vue";
-const email= ref('karim@gmail.com');
-const props = defineProps(['name', 'phone', 'ownername']);
+import { ref, defineProps } from "vue";
+// const email = ref("karim@gmail.com");
+const props = defineProps({
+name:{type:String, required:true},
+phone: Number,
+ownername: String,
+email: {type: String, require: false, default:'N/A'},
+isFavourite: Boolean,
+});
 </script>
