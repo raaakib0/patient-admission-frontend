3123942c8d6b8b68ceea832c720ab42dc0379e26
raaakib0 <raaakib0@gmail.com>
2025-11-01 19:10:51 +0600

    props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup></script>
 
 <template>
-  <contact-us></contact-us>
+  <!-- <contact-us></contact-us> -->
   <!-- <button-counter></button-counter> -->
+   <ContactUs
+   name="mr.x"
+   phone="01911111111"
+   :ownername="contactsOwner"
+   ></ContactUs>
   <ButtonCounter></ButtonCounter>
   
   <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind + Vue 🎉</h1>
@@ -12,7 +16,9 @@
 </template>
 
 <script setup>
+import ContactUs from './Components/ContactUs.vue';
 import ButtonCounter from './Components/ButtonCounter.vue';
+const contactsOwner = "abcd"
 </script>
 
 <style scoped></style>
--- src/Components/ContactUs.vue
+++ src/Components/ContactUs.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="bg-info">
 <h1>Contact Us</h1>
-<p>please sent an emil to abc@gmail.comn</p>
+<p>Name: {{ name }}</p>
+<p>Email: {{ email }}</p>
+<p>Phone: {{ phone }}</p>
+<p class="float-end small " v-if="ownername!=''">this info for {{ ownername }}</p>
     </div>
 </template>
+
+<script setup>
+import {ref, defineProps} from "vue";
+const email= ref('karim@gmail.com');
+const props = defineProps(['name', 'phone', 'ownername']);
+</script>
