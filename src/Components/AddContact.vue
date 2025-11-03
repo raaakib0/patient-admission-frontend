5d6e468ce36b305e52e0a12f1c5d7497d63c7943
raaakib0 <raaakib0@gmail.com>
2025-11-03 20:48:12 +0600

    add contact form add

--- src/Components/AddContact.vue
+++ src/Components/AddContact.vue
@@ -1,4 +1,40 @@
-<template></template>
+<template>
+  <div class="container border rounded p-2">
+    <div class="text-center text-white">Add New Contact</div>
+    <form @submit.prevent="addContact">
+      <div class="row">
+        <div class="col-4">
+          <input
+            type="text"
+            class="form-control"
+            v-model="contact.name"
+            placeholder="Enter Name "
+          />
+        </div>
+        <div class="col-4">
+          <input
+            type="text"
+            class="form-control"
+            v-model="contact.email"
+            placeholder="Enter email "
+          />
+        </div>
+        <div class="col-4">
+          <input
+            type="number"
+            class="form-control"
+            v-model="contact.phone"
+            placeholder="Enter number "
+          />
+        </div>
+        <div class="col-6 offset:-3 p-2">
+            <button type="submit" class="btn btn-secondary w-100" >Add Contact</button>
+
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
 
 <script setup>
 import { reactive } from "vue";
@@ -8,4 +44,11 @@ const contact = reactive({
   phone: "",
   email: "",
 });
+
+function addContact() {
+  console.log(contact);
+  contact.name = "";
+  contact.email = "";
+  contact.phone = "";
+}
 </script>
