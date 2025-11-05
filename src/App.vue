66df6b8fc328c7a08a1cd879dec380722816c6ec
raaakib0 <raaakib0@gmail.com>
2025-11-05 21:29:19 +0600

    restore previous version

--- src/App.vue
+++ src/App.vue
@@ -23,54 +23,6 @@
     </div>
     <ButtonCounter></ButtonCounter>
   </div>
-   <div class="bg-black text pt-3" :style="{ height: '100vh' }">
-    <h1 class="text-center text- success">ConcoPedia</h1>
-    <div class="container">
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = !newVersion"
-      >
-        Toggle Component
-      </button>
-      <br />
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = false"
-      >
-        Lucky Number V1
-      </button>
-      <button class="btn btn-primary text-black m-2" @click="newVersion = true">
-        Lucky Number V2
-      </button>
-      <br />
-      <component :is="currentComponenet" />
-    </div>
-    <ButtonCounter></ButtonCounter>
-  </div>
-  <div class="bg-black text pt-3" :style="{ height: '100vh' }">
-    <h1 class="text-center text- success">ConcoPedia</h1>
-    <div class="container">
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = !newVersion"
-      >
-        Toggle Component
-      </button>
-      <br />
-      <button
-        class="btn btn-primary text-black m-2"
-        @click="newVersion = false"
-      >
-        Lucky Number V1
-      </button>
-      <button class="btn btn-primary text-black m-2" @click="newVersion = true">
-        Lucky Number V2
-      </button>
-      <br />
-      <component :is="currentComponenet" />
-    </div>
-    <ButtonCounter></ButtonCounter>
-  </div>
 </template>
 
 <script setup>
