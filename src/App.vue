d483c3670270eaf1364a7b2a2d16f3e6ef538624
raaakib0 <raaakib0@gmail.com>
2025-11-06 20:54:53 +0600

    slot

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+<LuckyNumberPareantComponent></LuckyNumberPareantComponent>
+</template>
+
+<script setup>
+import LuckyNumberPareantComponent from './Components/LuckyNumberPareantComponent.vue';
+</script>
