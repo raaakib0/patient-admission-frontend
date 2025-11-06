9bf178414234235d1af0e57fb5f2540827dc103d
raaakib0 <raaakib0@gmail.com>
2025-11-06 21:19:42 +0600

    named slots

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,19 @@
 
 <p class="P-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, fugiat dolo</p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, fugiat dolo</p>
+<button @click="showMessage">what will we lern?</button>
 <hr/>
 </LuckyNumberPareantComponent>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import LuckyNumberPareantComponent from './Components/LuckyNumberPareantComponent.vue';
+
+const message = ref("");
+
+function showMessage(){
+  message.value= "how to use slots"
+}
+
 </script>
