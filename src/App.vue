430d4b53e706adb44612bfee057e1d21170efd2b
raaakib0 <raaakib0@gmail.com>
2025-11-08 20:59:07 +0600

    inc,dec

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-
+<button @click="increment">Increment</button>
+<button @click="decrement">Decrement</button>
 </template>
 
 <script setup>
+
+const count = ref(10);
+
+const increment = ()=>{
+  count.value++;
+};
+
+const decrement = ()=>{
+  count.value--;
+};
 
 </script>
