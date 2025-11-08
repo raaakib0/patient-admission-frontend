6a14f4553467e10457a273d24f6e9f76fd87429e
raaakib0 <raaakib0@gmail.com>
2025-11-08 21:34:26 +0600

    5.02

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1>{{ count }}</h1>
-<button @click="increment">Increment</button>
-<button @click="decrement">Decrement</button>
-<ButtonCounter></ButtonCounter>
+  <h1>hello</h1>
 </template>
 
 <script setup>
-import { useCounter } from './composibles/useCounter';
-import ButtonCounter from './Components/ButtonCounter.vue';
-const {count, increment, decrement} = useCounter(100,10);
 
 </script>
