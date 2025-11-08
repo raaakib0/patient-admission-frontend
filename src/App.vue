0c54175a4274aab7451916d340ccbf186b6e1fe2
raaakib0 <raaakib0@gmail.com>
2025-11-08 20:32:09 +0600

    function and button properly worked

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,47 @@
 <template>
-  <h1>Hello vue</h1>
+  <h1>{{ message }}</h1>
   <Button @click="showMessage">Show Message</Button>
- <ButtonCounter/>
+  <br />
+  <h1>{{ count }}</h1>
+  <button @click="count++">Clicked{{ count }} time</button>
+  <ButtonCounter v-if="count==1" />
 </template>
 
 <script setup>
-import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
-import ButtonCounter from './Components/ButtonCounter.vue';
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+} from "vue";
+import ButtonCounter from "./Components/ButtonCounter.vue";
 
-const message =ref("Hello Vue");
-const showMessage = () =>{
+const message = ref("Hello Vue");
+const showMessage = () => {
   message.value = "Hello from function";
 };
 
 const count = ref(0);
 
-onBeforeMount(()=>{
+onBeforeMount(() => {
   console.log("onBeforemount - App.vue");
 });
-onMounted(()=>{
-  console.log('onMounted - App.vue');
+onMounted(() => {
+  console.log("onMounted - App.vue");
 });
-onBeforeUpdate(()=>{
+onBeforeUpdate(() => {
   console.log("onBeforeUpdate - App.vue");
 });
-onUpdated(()=>{
-  console.log('onUpdated - App.vue');
+onUpdated(() => {
+  console.log("onUpdated - App.vue");
 });
-onBeforeUnmount(()=>{
-  console.log('onBeforeUnmount - App.vue');
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount - App.vue");
 });
-onUnmounted(()=>{
-  console.log('onUnmounted - App.vue');
+onUnmounted(() => {
+  console.log("onUnmounted - App.vue");
 });
-
 </script>
