4e0b9c1a9fb362c4b350abcc62bdb1f48770abf7
raaakib0 <raaakib0@gmail.com>
2025-11-08 20:50:12 +0600

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,7 @@
 <template>
-  <h1>{{ message }}</h1>
-  <Button @click="showMessage">Show Message</Button>
-  <br />
-  <h1>{{ count }}</h1>
-  <button @click="count++">Clicked{{ count }} time</button>
-  <ButtonCounter v-if="count==1" />
+
 </template>
 
 <script setup>
-import {
-  onBeforeMount,
-  onBeforeUnmount,
-  onBeforeUpdate,
-  onMounted,
-  onUnmounted,
-  onUpdated,
-  ref,
-} from "vue";
-import ButtonCounter from "./Components/ButtonCounter.vue";
 
-const message = ref("Hello Vue");
-const showMessage = () => {
-  message.value = "Hello from function";
-};
-
-const count = ref(0);
-
-onBeforeMount(() => {
-  console.log("onBeforemount - App.vue");
-});
-onMounted(() => {
-  console.log("onMounted - App.vue");
-});
-onBeforeUpdate(() => {
-  console.log("onBeforeUpdate - App.vue");
-});
-onUpdated(() => {
-  console.log("onUpdated - App.vue");
-});
-onBeforeUnmount(() => {
-  console.log("onBeforeUnmount - App.vue");
-});
-onUnmounted(() => {
-  console.log("onUnmounted - App.vue");
-});
 </script>
--- src/Components/ButtonCounter.vue
+++ src/Components/ButtonCounter.vue
@@ -1,36 +1,6 @@
 <template>
-    <button @click="count++" >Clicked {{ count }} times</button>
+<h1>Button Counter</h1>
 </template>
 <script>
-
-import { ref } from 'vue';
-
-export default{
-    setup(){
-        const count = ref(0);
-        return {count};
-    }
-};
-
-import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from 'vue';
-
-onBeforeMount(()=>{
-  console.log("onBeforemount - ButtonCounter.vue");
-});
-onMounted(()=>{
-  console.log('onMounted - ButtonCounter.vue');
-});
-onBeforeUpdate(()=>{
-  console.log("onBeforeUpdate - ButtonCounter.vue");
-});
-onUpdated(()=>{
-  console.log('onUpdated - ButtonCounter.vue');
-});
-onBeforeUnmount(()=>{
-  console.log('onBeforeUnmount - ButtonCounter.vue');
-});
-onUnmounted(()=>{
-  console.log('onUnmounted - ButtonCounter.vue');
-});
 
 </script>
