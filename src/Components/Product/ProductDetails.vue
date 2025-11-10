4c2aea0b04f6bdaa3b6eeb4656274e4007121e48
raaakib0 <raaakib0@gmail.com>
2025-11-10 21:45:51 +0600

    products details dinamic

--- src/Components/Product/ProductDetails.vue
+++ src/Components/Product/ProductDetails.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+    <div>
+        <h1>Products Details</h1>
+        <p>Products ID: {{ route.params.productID }}</p>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log(route.params);
+</script>
