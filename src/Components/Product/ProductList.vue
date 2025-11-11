1cdb51f2563325141a1c17e0eff52926a86cb8fb
raaakib0 <raaakib0@gmail.com>
2025-11-11 21:35:43 +0600

    dinamic products list

--- src/Components/Product/ProductList.vue
+++ src/Components/Product/ProductList.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <h1>Products List</h1>
+    <p>here is the list of products:</p>
+    <div v-for="product in products">
+      <router-link :to="`/product/${product.id}`">{{ product.name }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const products = [
+  { id: 1, name: "products-1" },
+  { id: 2, name: "products-2" },
+  { id: 3, name: "products-3" },
+];
+</script>
