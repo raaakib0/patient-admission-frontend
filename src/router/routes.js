a5aab5ba23da65b76630821139afdff06f05d7ca
raaakib0 <raaakib0@gmail.com>
2025-11-12 20:55:22 +0600

    redirect route

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,10 +9,12 @@ const router = createRouter({
     routes: [
         { path: "/", component: HomePage },
         { path: "/contact-us", component: Contact, name: 'contact' },
+        { path: '/contact', redirect: { name: 'contact' } },
         { path: "/productList", component: ProductList },
-        { path: "/product/:productId/:categoryId?", component: ProductDetails, name: 
-            'productDetails', props: true
-         },
+        {
+            path: "/product/:productId/:categoryId?", component: ProductDetails, name:
+                'productDetails', props: true
+        },
         { path: "/product", component: ProductDetails }
     ],
 });
