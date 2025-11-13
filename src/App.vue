014bd2edf532cd30b39733c19b85c56f200e259c
raaakib0 <raaakib0@gmail.com>
2025-11-13 20:09:21 +0600

    fetch

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <h1>hello</h1>
 
- <div v-for='user in userObj.users' :key="user.id">
-  <p>{{ user.name }}</p>
-  <p>{{ user.email }}</p>
-  <hr/>
- </div>
+  <div v-for="user in userObj.users" :key="user.id">
+    <p>{{ user.name }}</p>
+    <p>{{ user.email }}</p>
+    <hr />
+  </div>
 </template>
 
 <script setup>
-import { onMounted, reactive } from 'vue';
-import Header from './Components/Layout/Header.vue';
-import axios from 'axios';
+import { onMounted, reactive } from "vue";
+import Header from "./Components/Layout/Header.vue";
+import axios from "axios";
 
 const userObj = reactive({
-  user:[],
+  user: [],
 });
 
-onMounted(()=>{
-  axios.get('https://jsonplaceholder.typicode.com/users').then((response)=>{
-    console.log(response.data);
+onMounted(() => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data)=>{
+    console.log(data);
+    userObj.users= data;
+  });
 
-    userObj.users= response.data;
-  })
-})
+  // axios.get("https://jsonplaceholder.typicode.com/users")
+  // .then((response) => {
+  //   console.log(response.data);
 
+  //   userObj.users = response.data;
+  // });
+});
 </script>
