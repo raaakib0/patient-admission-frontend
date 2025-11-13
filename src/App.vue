ef45f6b125b38486a6257e17ed1c381d2a7ecf3f
raaakib0 <raaakib0@gmail.com>
2025-11-13 20:00:36 +0600

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <h1>hello</h1>
 
-  <Header></Header>
-  <router-view></router-view>
+ <div v-for='user in userObj.users' :key="user.id">
+  <p>{{ user.name }}</p>
+  <p>{{ user.email }}</p>
+  <hr/>
+ </div>
 </template>
 
 <script setup>
+import { onMounted, reactive } from 'vue';
 import Header from './Components/Layout/Header.vue';
+import axios from 'axios';
 
-Header
+const userObj = reactive({
+  user:[],
+});
+
+onMounted(()=>{
+  axios.get('https://jsonplaceholder.typicode.com/users').then((response)=>{
+    console.log(response.data);
+
+    userObj.users= response.data;
+  })
+})
+
 </script>
