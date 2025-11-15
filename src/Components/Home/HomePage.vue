554b9e52fa1f4ff40aa12ec403101cd6cde07cf5
raaakib0 <raaakib0@gmail.com>
2025-11-15 21:25:55 +0600

    write function

--- src/Components/Home/HomePage.vue
+++ src/Components/Home/HomePage.vue
@@ -1,26 +1,37 @@
 <template>
-<div class="text-center">
-<p class=" pt-3">Let's Play The Game</p>
-<h2 class="text-primary pb-3">Current Score: {{ gameStore.score }}</h2>
-<span class="text-primary pb-3 ">Max Score:{{ gameStore.maxHeat }}</span>
-<br/>
-<div class="row">
-    <div class="col-5 offset-1">
+  <div class="text-center">
+    <p class="pt-3">Let's Play The Game</p>
+    <h2 class="text-primary pb-3">Current Score: {{ gameStore.score }}</h2>
+    <span class="text-primary pb-3">Max Score:{{ gameStore.maxHeat }}</span>
+    <br />
+    <div class="row">
+      <div class="col-5 offset-1">
         <button class="form-control btn btn-success p-3">Increment</button>
-    </div>
-    <div class="col-5 ">
+      </div>
+      <div class="col-5">
         <button class="form-control btn btn-danger p-3">Decrement</button>
-    </div>
-    <div class="col-6  pt-2 offset-3">
+      </div>
+      <div class="col-6 pt-2 offset-3">
         <button class="form-control btn btn-warning p-3">Random</button>
+      </div>
     </div>
-</div>
-
-</div>
+  </div>
 </template>
 
 <script setup>
-import { useGameStore } from '@/Store/GameStore';
+import { useGameStore } from "@/Store/GameStore";
 
-const gameStore= useGameStore();
+function Increment() {
+  gameStore.score += Math.floor(Math.random() * 30) + 1;
+}
+
+function Decrement() {
+  gameStore.score += Math.floor(Math.random() * 10) + 1;
+}
+
+function Random(){
+    Math.random()>0.5 ?Increment(): Decrement();
+}
+
+const gameStore = useGameStore();
 </script>
